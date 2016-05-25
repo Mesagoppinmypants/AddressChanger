7d3d08a4b3625d2bb45c19b0756cc3397e6e6b9c
Gene Pressinger <genepressinger@yahoo.com>
2016-05-25 18:31:56 -0400

    Part of previous commit

--- AddressChanger/AddressChanger/MainForm.cs
+++ AddressChanger/AddressChanger/MainForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,40 @@ namespace AddressChanger
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            string swgDir = Properties.Settings.Default.swgDirectory;
+            if (Directory.Exists(swgDir))
+            {
+                StreamWriter configWriter = new StreamWriter(swgDir + @"\user.cfg");
+            }
+            else
+                MessageBox.Show("Please enter a valid SWG game directory.", "Invalid Directory");
+        }
+
+        private void changeDirButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog swgDirBrowser = new FolderBrowserDialog();
+            swgDirBrowser.Description = "Please show us where your SWG game directory is.";
+            if (swgDirBrowser.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(swgDirBrowser.SelectedPath + @"\SwgClient_r.exe"))
+                {
+                    Properties.Settings.Default.swgDirectory = swgDirBrowser.SelectedPath;
+                    Properties.Settings.Default.Save();
+                    gameDirectoryTextBox.Text = Properties.Settings.Default.swgDirectory;
+                }
+                else
+                    MessageBox.Show("Invalid SWG NGE game directory.", "Invalid Directory");
+            }
+        }
+
+        private void openDirButton_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Properties.Settings.Default.swgDirectory))
+                Process.Start(Properties.Settings.Default.swgDirectory);
         }
     }
 }
